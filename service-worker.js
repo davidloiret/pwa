8deb431449bc5e3dbacb014a24eb3e020d27f9e2
David Loiret <d.loiret@gatewatcher.com>
2022-06-16 14:53:02 +0200

    Add cache invalidation

--- service-worker.js
+++ service-worker.js
@@ -21,6 +21,20 @@ self.addEventListener('install', event => {
   )
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys => 
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_KEY)
+            .map(key => caches.delete(key))
+        )
+      )
+  )
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(response => {
